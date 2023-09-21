afe8e52dc70c4812263aa39584b1a858d3634c73
Daniel Souza <dsouza1995@gmail.com>
2023-09-21 16:41:27 -0300

    feat: add methods to get previous and next words

--- src/syncfusion/index.ts
+++ src/syncfusion/index.ts
@@ -1,6 +1,11 @@
-import type { Editor, EditorHistory } from "@syncfusion/ej2-documenteditor";
+import type { Editor, EditorHistory, Selection } from "@syncfusion/ej2-documenteditor";
 import { EditorAdapter } from "../editor";
 import { IaraInference } from "../speech";
+
+interface SelectionOffsets {
+  end: string; 
+  start: string;
+}
 
 export class IaraSyncfusionAdapter
   extends EditorAdapter
@@ -14,6 +19,34 @@ export class IaraSyncfusionAdapter
 
   private get _editorHistory(): EditorHistory {
     return this._editor.editorHistory;
+  }
+
+  private get _editorSelection(): Selection {
+    return this._editor.selection;
+  }
+
+  private _addTrailingSpaces(text: string) {    
+    const initialSelectionOffsets = {end: this._editorSelection.endOffset, start: this._editorSelection.startOffset};
+    const wordAfter = this._getWordAfterSelection(initialSelectionOffsets);
+    const wordBefore = this._getWordBeforeSelection(initialSelectionOffsets);
+
+    const prefix = (wordBefore.length && !wordBefore.endsWith(' ')) ? ' ': '';
+    const suffix = (wordAfter.length && !wordAfter.startsWith(' ')) ? ' ': '';
+    return `${prefix}${text}${suffix}`;
+  }
+
+  private _getWordAfterSelection(selectionOffsets: SelectionOffsets): string {
+    this._editorSelection.extendToWordEnd();
+    const wordAfter = this._editorSelection.text.trimEnd();
+    this._editorSelection.select(selectionOffsets.start, selectionOffsets.end);
+    return wordAfter;
+  }
+
+  private _getWordBeforeSelection(selectionOffsets: SelectionOffsets): string {
+    this._editorSelection.extendToWordStart();
+    const wordBefore = this._editorSelection.text.trimStart();
+    this._editorSelection.select(selectionOffsets.start, selectionOffsets.end);
+    return wordBefore;
   }
 
   getUndoStackSize(): number {
@@ -30,23 +63,25 @@ export class IaraSyncfusionAdapter
 
   insertInference(inference: IaraInference) {
     if (inference.isFirst) {
+      if (this._editorSelection.text.length) this._editorAPI.delete();
       this._initialUndoStackSize = this.getUndoStackSize();
     } else {
       const undoStackSize = this.getUndoStackSize();
       for (let i = 0; i < undoStackSize - this._initialUndoStackSize; i++)
-        this.undo();
+      this.undo();
     }
 
-    const text = inference.richTranscript
+    let text = inference.richTranscript
       .replace(/^<div>/, "")
       .replace(/<\/div>$/, "");
-    const [firstLine, ...lines]: string[] = text.split("</div><div>");
-
+    text = this._addTrailingSpaces(text);
+   
+    let [firstLine, ...lines]: string[] = text.split("</div><div>");
     this.insertText(firstLine);
 
     lines.forEach(line => {
       this.insertParagraph();
-      line = line.trim();
+      line = line.trimStart();
       if (line) this.insertText(line);
     });
   }
