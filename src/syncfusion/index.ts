e58956296a2e58caf8fc47b3f5503a8b022d01c7
Daniel Souza <dsouza1995@gmail.com>
2023-09-25 20:30:36 -0300

    feat: handle trailing spaces and capitalization (#10)
    * feat: add methods to get previous and next words
    
    * feat: handle capitalization and more space cases

--- src/syncfusion/index.ts
+++ src/syncfusion/index.ts
@@ -1,12 +1,14 @@
-import type { Editor, EditorHistory } from "@syncfusion/ej2-documenteditor";
+import type { Editor, EditorHistory, Selection } from "@syncfusion/ej2-documenteditor";
 import { EditorAdapter } from "../editor";
 import { IaraInference } from "../speech";
+import { IaraSyncfusionInferenceFormatter } from "./formatter";
 
 export class IaraSyncfusionAdapter
   extends EditorAdapter
   implements EditorAdapter
 {
   private _initialUndoStackSize = 0;
+  private _inferenceFormatter: IaraSyncfusionInferenceFormatter;
 
   private get _editorAPI(): Editor {
     return this._editor.editor;
@@ -14,6 +16,15 @@ export class IaraSyncfusionAdapter
 
   private get _editorHistory(): EditorHistory {
     return this._editor.editorHistory;
+  }
+
+  private get _editorSelection(): Selection {
+    return this._editor.selection;
+  }
+
+  constructor(protected _editor: any, protected _recognition: any) {
+    super(_editor, _recognition);
+    this._inferenceFormatter = new IaraSyncfusionInferenceFormatter(this._editorSelection);
   }
 
   getUndoStackSize(): number {
@@ -30,23 +41,22 @@ export class IaraSyncfusionAdapter
 
   insertInference(inference: IaraInference) {
     if (inference.isFirst) {
+      if (this._editorSelection.text.length) this._editorAPI.delete();
       this._initialUndoStackSize = this.getUndoStackSize();
     } else {
       const undoStackSize = this.getUndoStackSize();
       for (let i = 0; i < undoStackSize - this._initialUndoStackSize; i++)
-        this.undo();
+      this.undo();
     }
 
-    const text = inference.richTranscript
-      .replace(/^<div>/, "")
-      .replace(/<\/div>$/, "");
-    const [firstLine, ...lines]: string[] = text.split("</div><div>");
-
+    const text = this._inferenceFormatter.format(inference);
+   
+    let [firstLine, ...lines]: string[] = text.split("</div><div>");
     this.insertText(firstLine);
 
     lines.forEach(line => {
       this.insertParagraph();
-      line = line.trim();
+      line = line.trimStart();
       if (line) this.insertText(line);
     });
   }
