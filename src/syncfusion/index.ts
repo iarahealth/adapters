0d082f3785d24f1bae5af1d0355d211d6f277e13
André Canuto <andrecanuto@MacBook-Pro-de-Andre.local>
2023-09-26 18:01:47 -0300

    feat:get editor content

--- src/syncfusion/index.ts
+++ src/syncfusion/index.ts
@@ -50,32 +50,10 @@ export class IaraSyncfusionAdapter
     this._editorAPI.insertText(text);
   }
 
-  async converter(template: string, type: 'html' | 'sfdt') {
-    const { Authorization } = this._recognitionInternal.iaraAPIMandatoryHeaders;
-    let htmlContent: any = type == 'html' ? { html: template } : { sfdt: template };
-    let endpoint = 'https://api.iarahealth.com/speech/syncfusion/';
-    endpoint += type == 'html' ? 'html_to_sfdt/' : 'sfdt_to_html/';
-
-    var headers = new Headers();
-    headers.append("Content-Type", "application/json;charset=UTF-8");
-    headers.append("Authorization", Authorization);
-
-    const response = await fetch(endpoint, {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify(htmlContent),
-      })
-      .then(async (response) => await response.json())
-      .catch(function (error) {
-        console.error(error.message)
-      });
-
-    return response;
-  }
-
-  async insertTemplate(template: string) {
-    const response = await this.converter(template, 'html');
-    this._editor.open(response);
+  async getEditorContent() {
+    const content = await this._editor.saveAsBlob("Sfdt")
+      .then((blob: Blob) => blob.text());
+    return content;
   }
 
   insertInference(inference: IaraInference) {
