e80f15910b0785917182831e0d0c309b934e804d
André Canuto <andrecanuto@MacBook-Pro-de-Andre.local>
2023-09-26 17:16:28 -0300

    feat:1769 - Insert template in editor

--- src/syncfusion/index.ts
+++ src/syncfusion/index.ts
@@ -16,6 +16,10 @@ export class IaraSyncfusionAdapter
     return this._editor.editorHistory;
   }
 
+  private get _recognitionInternal(): any {
+    return this._recognition.internal;
+  }
+
   getUndoStackSize(): number {
     return this._editorHistory.undoStack?.length || 0;
   }
@@ -26,6 +30,34 @@ export class IaraSyncfusionAdapter
 
   insertText(text: string) {
     this._editorAPI.insertText(text);
+  }
+
+  async converter(template: string, type: 'html' | 'sfdt') {
+    const { Authorization } = this._recognitionInternal.iaraAPIMandatoryHeaders;
+    let htmlContent: any = type == 'html' ? { html: template } : { sfdt: template };
+    let endpoint = 'https://api.iarahealth.com/speech/syncfusion/';
+    endpoint += type == 'html' ? 'html_to_sfdt/' : 'sfdt_to_html/';
+
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json;charset=UTF-8");
+    headers.append("Authorization", Authorization);
+
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(htmlContent),
+      })
+      .then(async (response) => await response.json())
+      .catch(function (error) {
+        console.error(error.message)
+      });
+
+    return response;
+  }
+
+  async insertTemplate(template: string) {
+    const response = await this.converter(template, 'html');
+    this._editor.open(response);
   }
 
   insertInference(inference: IaraInference) {
@@ -41,7 +73,7 @@ export class IaraSyncfusionAdapter
       .replace(/^<div>/, "")
       .replace(/<\/div>$/, "");
     const [firstLine, ...lines]: string[] = text.split("</div><div>");
-
+    
     this.insertText(firstLine);
 
     lines.forEach(line => {
