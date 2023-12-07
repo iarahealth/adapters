00865daa27571553357ca5e432ac4086d0bf74ec
Itallo Dornelas <72089253+ItalloDornelas@users.noreply.github.com>
2023-12-07 13:06:50 -0300

    Feature/prt 1772 1 (#31)
    * feat: add shortcuts
    
    * feat: add shortcuts manager
    
    * feat: order by template first
    
    * merge dev
    
    * feat: add init method shortcutsManeger
    
    * feat: improve modal
    
    * fix: add correct type

---
+++ src/syncfusion/shortcuts/index.ts
@@ -0,0 +1,64 @@
+import {
+  DocumentEditorContainer,
+  DocumentEditorKeyDownEventArgs,
+} from "@syncfusion/ej2-documenteditor";
+import { ListView } from "@syncfusion/ej2-lists";
+import { Dialog } from "@syncfusion/ej2-popups";
+import { IaraSpeechRecognition } from "../../speech";
+import { IaraSyncfusionTemplateSearch } from "./templateSearch";
+
+export class IaraSyncfusionShortcutsManager {
+  constructor(
+    private _editor: DocumentEditorContainer,
+    private _recognition: IaraSpeechRecognition,
+    private onTemplateSelected: (
+      listViewInstance: ListView,
+      dialogObj: Dialog
+    ) => void
+  ) {}
+
+  init(): void {
+    this._editor.documentEditor.keyDown = this.onKeyDown.bind(this);
+  }
+  onKeyDown(args: DocumentEditorKeyDownEventArgs): void {
+    const key: string = args.event.key;
+    const isShiftKey: boolean =
+      args.event.shiftKey || args.event.metaKey
+        ? true
+        : key === "Shift"
+        ? true
+        : false;
+
+    if (isShiftKey && key === "@") {
+      const templates = [
+        ...Object.values(this._recognition.richTranscriptTemplates.templates),
+      ];
+
+      const updateFormatTemplates = templates.map(template => {
+        const categoryName = template.metadata as { category: string };
+        return {
+          name: template.key,
+          category: categoryName.category ? categoryName.category : "",
+          content: template.replaceText,
+        };
+      });
+
+      const sortedData = updateFormatTemplates.sort(
+        (oldTemplate, newTemplate) => {
+          // Compare based on the 'type' key
+          if (oldTemplate.category === newTemplate.category) {
+            // If types are the same, order by the 'value' key
+            return oldTemplate["name"].localeCompare(newTemplate["name"]);
+          } else {
+            // Order 'template' items first
+            return oldTemplate.category === "template" ? -1 : 1;
+          }
+        }
+      );
+      new IaraSyncfusionTemplateSearch(
+        sortedData ? sortedData : updateFormatTemplates,
+        this.onTemplateSelected
+      );
+    }
+  }
+}
---
+++ src/syncfusion/shortcuts/templateSearch.ts
@@ -0,0 +1,70 @@
+import { DataManager, Query } from "@syncfusion/ej2-data";
+import { ListView } from "@syncfusion/ej2-lists";
+import { Dialog, DialogUtility } from "@syncfusion/ej2-popups";
+
+export class IaraSyncfusionTemplateSearch {
+  private _template: string;
+  private _listviewInstance: ListView;
+  constructor(
+    private _dataSource: { name: string; category: string; content: string }[],
+    private _onTemplateSelected: (
+      listViewInstance: ListView,
+      dialogObj: Dialog
+    ) => void
+  ) {
+    this._template =
+      '<div class="e-list-wrapper" style="padding-left:10px; overflow: auto; height: 120px;">' +
+      '<div class="e-list-wrappere-list-multi-line">' +
+      '<span class="e-list-item-header">${name}</span>' +
+      "</div>" +
+      "</div>";
+
+    this._listviewInstance = new ListView({
+      dataSource: this._dataSource,
+      sortOrder: "None",
+      fields: { text: "name", groupBy: "category" },
+      template: this._template,
+      groupTemplate:
+        '<div><span class="category">${items[0].category}</span> <span class="count"> ${items.length} Item(s)</span></div> ',
+    });
+
+    const dialogObj = DialogUtility.alert({
+      title: "<div class='dlg-template'>Selecione a frase/template</div>",
+      content: `<div class="e-list-wrapper" >
+      <input class="e-input" type="text" id="textbox" placeholder="Buscar" title="Type in a name">
+      <div id='listview' style="overflow: auto; max-height: 300px;"></div>
+      </div>`,
+      width: "350px",
+      showCloseIcon: true,
+      closeOnEscape: true,
+      okButton: { text: "" },
+    });
+    this._listviewInstance.appendTo("#listview");
+    document.getElementById("textbox")?.addEventListener("keyup", () => {
+      this.filter(this._listviewInstance, this._dataSource);
+    });
+    this._onTemplateSelected(this._listviewInstance, dialogObj);
+  }
+
+  filter(
+    listObj: ListView,
+    listData: { name: string; category: string; content: string }[]
+  ): void {
+    if (document.getElementById("textbox")) {
+      const value = (document.getElementById("textbox") as HTMLInputElement)
+        .value;
+      const data: unknown[] = new DataManager(listData).executeLocal(
+        new Query().where("name", "startswith", value, true)
+      );
+      if (!value) {
+        listObj.dataSource = listData.slice();
+      } else {
+        listObj.dataSource = data as {
+          [key: string]: Record<string, unknown>;
+        }[];
+      }
+      listObj.dataBind();
+    }
+    return;
+  }
+}
