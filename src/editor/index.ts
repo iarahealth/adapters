28411423dd7d1eb458f6edd3160a998272377a85
Itallo Dornelas <72089253+ItalloDornelas@users.noreply.github.com>
2023-12-23 11:48:14 -0300

    Feature/prt 1761 1 (#49)
    * feat: improve debounce logic to save
    
    * feat: add rich and text to get id
    
    * fix: add id undefined when begin
    
    * fix: remove log

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -59,6 +59,7 @@ export abstract class EditorAdapter {
   abstract getEditorContent(): Promise<[string, string, string]>;
 
   beginReport(): string | void {
+    this._recognition.report["_key"] = "";
     return this._recognition.beginReport({ richText: "", text: "" });
   }
 
