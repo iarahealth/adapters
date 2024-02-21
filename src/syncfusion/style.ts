080b45cca0c1dc7f62fc1d682ccb4c1724587c67
Augusto Braun <augusto@iarahealth.com>
2024-02-21 16:25:39 -0300

    versao testando o fundo dark carregando light

--- src/syncfusion/style.ts
+++ src/syncfusion/style.ts
@@ -29,22 +29,92 @@ export class IaraSyncfusionStyleManager extends IaraEditorStyleManager {
     this._editor.focusIn();
   }
   setTheme(theme: "light" | "dark") {
-    if (theme === "light") return;
+    // if (theme === "slight") return;
+    if (theme === "dark")
+    {
+      this._editor.documentHelper.backgroundColor = "#444";
+      this._editor.setDefaultCharacterFormat({ fontColor: "#fff" });
+      return;
+    }
+
+    const header = document.getElementsByTagName("head")[0];
+
+    const styleNode = document.createElement("style");
+    header.appendChild(styleNode);
+    styleNode.sheet?.insertRule(".e-de-background {background-color: #fafafa !important;}");
+    styleNode.sheet?.insertRule(".body:not(.is-mobile) ::-webkit-scrollbar  {background-color: inherit !important;}");
+    // styleNode.sheet?.insertRule(".e-documenteditor-optionspane {display: block !important;}");
+    // styleNode.sheet?.insertRule("body.theme-iara {color: white; background-color: #fafafa !important;}");
+    // styleNode.sheet?.insertRule(".e-de-blink-cursor { border-left: 1px solid #fff !important; }");
+    // styleNode.sheet?.insertRule(".e-de-status-bar span { color: #fff !important; }");
+
+    // this._editor.documentHelper.backgroundColor = "#fafafa";
+    // this._editor.setDefaultCharacterFormat({ fontColor: "#fff" });
+
+    const cssFile = "https://cdn.syncfusion.com/ej2/22.1.34/material3.css";
+    const css = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("id", "light-theme-css");
+    css.setAttribute("href", cssFile);
+    header.appendChild(css);
+
+    const cssFile2 = "https://cdn.syncfusion.com/ej2/22.1.34/material.css";
+    const css2 = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("id", "base-ribbon-css");
+    css.setAttribute("href", cssFile2);
+    header.appendChild(css2);
+
+
+    const cssFile3 = "https://cdn.jsdelivr.net/npm/@syncfusion/ej2-ribbon@24.2.3/styles/material.min.css";
+    const css3 = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("id", "light-ribbon-css");
+    css.setAttribute("href", cssFile3);
+    header.appendChild(css3);
+
+    // @import "@syncfusion/ej2-ribbon/styles/material3-dark.css";
+
+    /* const header = document.getElementsByTagName("head")[0];
+
+    const styleNode = document.createElement("style");
+    header.appendChild(styleNode);
+    // styleNode.sheet?.insertRule(".e-de-background {background-color: #302D38 !important;}");
+    // styleNode.sheet?.insertRule("body.theme-iara {color: white; background-color: #212429 !important;}");
+    styleNode.sheet?.insertRule(".e-de-blink-cursor { border-left: 1px solid #fff !important; }");
+    styleNode.sheet?.insertRule(".e-de-status-bar span { color: #fff !important; }");
 
     this._editor.documentHelper.backgroundColor = "#444";
     this._editor.setDefaultCharacterFormat({ fontColor: "#fff" });
-    const FILE = "https://cdn.syncfusion.com/ej2/22.1.34/material3-dark.css";
+
+    const cssFile = "https://cdn.syncfusion.com/ej2/22.1.34/material3-dark.css";
     const css = document.createElement("link");
     css.setAttribute("rel", "stylesheet");
     css.setAttribute("type", "text/css");
     css.setAttribute("id", "dark-theme-css");
-    css.setAttribute("href", FILE);
-    document.getElementsByTagName("head")[0].appendChild(css);
+    css.setAttribute("href", cssFile);
+    header.appendChild(css); */
 
-    const styleNode = document.createElement("style");
-    document.getElementsByTagName("head")[0].appendChild(styleNode);
-    styleNode.sheet?.insertRule(".e-de-blink-cursor { border-left: 1px solid #fff !important; }");
-    styleNode.sheet?.insertRule(".e-de-status-bar span { color: #fff !important; }");
+
+
+
+    // this._editor.documentHelper.backgroundColor = "#444";
+    // this._editor.setDefaultCharacterFormat({ fontColor: "#fff" });
+    // const FILE = "https://cdn.syncfusion.com/ej2/22.1.34/material3-dark.css";
+    // const css = document.createElement("link");
+    // css.setAttribute("rel", "stylesheet");
+    // css.setAttribute("type", "text/css");
+    // css.setAttribute("id", "dark-theme-css");
+    // css.setAttribute("href", FILE);
+    // document.getElementsByTagName("head")[0].appendChild(css);
+
+    // const styleNode = document.createElement("style");
+    // document.getElementsByTagName("head")[0].appendChild(styleNode);
+    // styleNode.sheet?.insertRule(".e-de-blink-cursor { border-left: 1px solid #fff !important; }");
+    // styleNode.sheet?.insertRule(".e-de-status-bar span { color: #fff !important; }");
   }
 
   toggleBold(): void {
