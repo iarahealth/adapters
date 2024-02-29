2fd950780034fe5f887bd7046665331c441f497b
Itallo <dornelaszxd@gmail.com>
2024-02-29 09:25:24 -0300

    feat: add translate and heitgh of menu ribbon

--- src/syncfusion/toolbar/index.ts
+++ src/syncfusion/toolbar/index.ts
@@ -82,22 +82,24 @@ export class IaraSyncfusionToolbarManager {
       ".e-ribbon-group-content"
     );
     if (layout === "Classic") {
-      editorContainerViewer.style.height = "calc(100% - 156px)";
+      editorContainerViewer.style.height = "calc(100% - 161px)";
       ribbonGroupContent.forEach(ribbon => {
         const ribbons = ribbon as HTMLElement;
         ribbons.style.height = "88px";
       });
       ribbonContentElement.style.height = "120px";
       ribbonExpandedMenuElement.style.height = "120px";
+      this._editorContainer.resize();
     }
     if (layout === "Simplified") {
-      editorContainerViewer.style.height = "calc(100% - 77px)";
+      editorContainerViewer.style.height = "calc(100% - 82px)";
       ribbonGroupContent.forEach(ribbon => {
         const ribbons = ribbon as HTMLElement;
         ribbons.style.height = "auto";
       });
       ribbonContentElement.style.height = "auto";
       ribbonExpandedMenuElement.style.height = "auto";
+      this._editorContainer.resize();
     }
   }
 }
