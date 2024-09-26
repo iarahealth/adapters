a7ca04787cf3d76e08ceec9b4b33f5451e54ea0a
Itallo Dornelas <72089253+ItalloDornelas@users.noreply.github.com>
2024-09-26 11:48:28 -0300

    feat: improve logic to use additive fields (#187)

--- src/syncfusion/navigationFields/additive.ts/list.ts
+++ src/syncfusion/navigationFields/additive.ts/list.ts
@@ -3,29 +3,15 @@ import { IaraSyncfusionNavigationFieldManager } from "..";
 import { IaraAdditiveBookmark } from "../navigationBookmark";
 
 export class IaraSyncfusionAdditiveList {
-  constructor(
-    private _instance: IaraSyncfusionNavigationFieldManager,
-    private _additiveField: IaraAdditiveBookmark,
-    private _additiveId: string
-  ) {
-    this.create();
-  }
+  private _list: ListView = new ListView();
+  private _fieldsSelected: string[] = [];
 
-  create = () => {
-    const contextMenuElement = document.createElement("ul");
-    contextMenuElement.id = "contextmenu-iara";
-    this._instance._documentEditor.documentHelper.viewerContainer.appendChild(
-      contextMenuElement
-    );
-    const menuItems = this._additiveField.additiveTexts.map(text => {
-      return {
-        text: text.identifier,
-        value: text.phrase,
-      };
-    });
+  constructor(private _instance: IaraSyncfusionNavigationFieldManager) {}
+
+  create = (additiveField: IaraAdditiveBookmark, additiveId: string) => {
     let fieldSelected: string[] = [];
-    const listObj: ListView = new ListView({
-      dataSource: menuItems,
+    this._list = new ListView({
+      dataSource: this.createItems(additiveField),
       showCheckBox: true,
       width: "200px",
       select: (args: {
@@ -33,53 +19,62 @@ export class IaraSyncfusionAdditiveList {
         isChecked: boolean;
       }) => {
         if (args.isChecked) {
-          this._instance.selectBookmark(this._additiveId);
-          if (!fieldSelected.includes(`${this._additiveId}-${args.data.text}`))
+          this._instance.selectBookmark(additiveId);
+          if (!fieldSelected.includes(`${additiveId}-${args.data.text}`))
             fieldSelected = [...fieldSelected, args.data.value];
         } else {
           fieldSelected = fieldSelected.filter(additive => {
             return (
-              `${this._additiveId}-${additive}` !==
-              `${this._additiveId}-${args.data.text}`
+              `${additiveId}-${additive}` !== `${additiveId}-${args.data.text}`
             );
           });
         }
       },
     });
-    listObj.appendTo("#contextmenu-iara");
-    this.customPosition(listObj);
-    this.customStyle(listObj);
+    const contextMenuElement = document.createElement("ul");
+    contextMenuElement.id = "contextmenu-iara";
+    this._instance._documentEditor.documentHelper.viewerContainer.appendChild(
+      contextMenuElement
+    );
+
+    this._list.appendTo("#contextmenu-iara");
+    this.customPosition();
+    this.customStyle();
     const btn = this.customButton(contextMenuElement);
-    btn.onclick = () => this.addContent(fieldSelected);
+    btn.onclick = () => this.addContent(fieldSelected, additiveField);
+    this._instance._documentEditor.isReadOnly = true;
   };
 
-  addContent = (fieldSelected: string[]) => {
-    this.closeList();
+  addContent = (
+    fieldSelected: string[],
+    additiveField: IaraAdditiveBookmark
+  ) => {
+    this.hide();
     fieldSelected.map((fileChecked, index) => {
       this._instance._documentEditor.editor.insertText(fileChecked);
       if (fieldSelected.length - 2 === index) {
         this._instance._documentEditor.editor.insertText(
-          ` ${this._additiveField.delimiterEnd} `
+          ` ${additiveField.delimiterEnd} `
         );
       } else if (fieldSelected.length - 1 !== index) {
         this._instance._documentEditor.editor.insertText(
-          `${this._additiveField.delimiterStart} `
+          `${additiveField.delimiterStart} `
         );
       }
     });
   };
 
-  customStyle = (listObj: ListView) => {
-    listObj.element.style.margin = "0";
-    listObj.element.style.padding = "0";
-    listObj.element.style.zIndex = "999";
-    listObj.element.style.borderColor = "#BAE1FE";
-    listObj.element.style.borderRadius = "5px";
-    listObj.element.style.borderStyle = "dotted";
-    listObj.element.style.borderWidth = "1px";
+  customStyle = () => {
+    this._list.element.style.margin = "0";
+    this._list.element.style.padding = "0";
+    this._list.element.style.zIndex = "999";
+    this._list.element.style.borderColor = "#BAE1FE";
+    this._list.element.style.borderRadius = "5px";
+    this._list.element.style.borderStyle = "dotted";
+    this._list.element.style.borderWidth = "1px";
   };
 
-  customPosition = (listObj: ListView) => {
+  customPosition = () => {
     const documentEditorContainer =
       this._instance._documentEditor.element.getBoundingClientRect();
     const textPosition =
@@ -89,9 +84,9 @@ export class IaraSyncfusionAdditiveList {
     const yPos = rulerOffset.top + textPosition.y;
     const xPos =
       rulerOffset.left + textPosition.x - documentEditorContainer.left;
-    listObj.element.style.position = "absolute";
-    listObj.element.style.top = `${yPos}px`;
-    listObj.element.style.left = `${xPos}px`;
+    this._list.element.style.position = "absolute";
+    this._list.element.style.top = `${yPos}px`;
+    this._list.element.style.left = `${xPos}px`;
   };
 
   customButton = (contextMenuElement: HTMLElement) => {
@@ -107,14 +102,61 @@ export class IaraSyncfusionAdditiveList {
     return button;
   };
 
-  closeList = () => {
+  createItems = (additiveField: IaraAdditiveBookmark) => {
+    return additiveField.additiveTexts.map(text => {
+      return {
+        text: text.identifier,
+        value: text.phrase,
+      };
+    });
+  };
+
+  selectItem = (
+    args: {
+      data: { text: string; value: string };
+      isChecked: boolean;
+    },
+    additiveId: string
+  ) => {
+    if (args.isChecked) {
+      this._instance.selectBookmark(additiveId);
+      if (!this._fieldsSelected.includes(`${additiveId}-${args.data.value}`))
+        this._fieldsSelected = [...this._fieldsSelected, args.data.value];
+    } else {
+      this._fieldsSelected = this._fieldsSelected.filter(additive => {
+        return (
+          `${additiveId}-${additive}` !== `${additiveId}-${args.data.value}`
+        );
+      });
+    }
+  };
+
+  hide = () => {
     const contextMenuElement = document.getElementById(
       "contextmenu-iara"
     ) as HTMLElement;
     if (contextMenuElement) {
-      contextMenuElement.style.display = "none";
       contextMenuElement.remove();
     }
     this._instance._documentEditor.isReadOnly = false;
+  };
+
+  show = (additiveField: IaraAdditiveBookmark, additiveId: string) => {
+    const contextMenuElement = document.createElement("ul");
+    contextMenuElement.id = "contextmenu-iara";
+    this._instance._documentEditor.documentHelper.viewerContainer.appendChild(
+      contextMenuElement
+    );
+    this._list.dataSource = this.createItems(additiveField);
+    this._list.appendTo("#contextmenu-iara");
+    this._list.select(
+      (args: { data: { text: string; value: string }; isChecked: boolean }) =>
+        this.selectItem(args, additiveId)
+    );
+    this.customPosition();
+    this.customStyle();
+    const btn = this.customButton(contextMenuElement);
+    btn.onclick = () => this.addContent(this._fieldsSelected, additiveField);
+    this._instance._documentEditor.isReadOnly = true;
   };
 }
