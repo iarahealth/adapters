bd595e5bdb7c049ae304f09bf267698a75ed9208
Daniel Souza <dsouza1995@gmail.com>
2024-09-28 10:14:47 -0300

    feat: import components lib script dinamically

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,11 @@ export * from "./editor";
 export * from "./speech";
 export * from "./syncfusion";
 export * from "./tinymce";
+
+const script = document.createElement("script");
+script.setAttribute(
+  "src",
+  "https://components-lib-git-feat-assistant-remake-iarahealth.vercel.app/components-lib.js"
+);
+script.setAttribute("type", "module");
+document.head.appendChild(script);
